d33aee1d825e9c36ffb9c9efde458c4c6fab8f63
Shelgunov Dmitriy <shelgunovdima3@gmail.com>
2021-06-23 17:18:54 +0600

    feat: unique and check constraint add for playlist-to-content

--- src/playlist-to-contents/playlist-to-content.entity.ts
+++ src/playlist-to-contents/playlist-to-content.entity.ts
@@ -1,29 +1,39 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Content } from 'src/contents/content.entity';
 import { Playlist } from 'src/playlists/playlist.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Check,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 const tableName = 'playlist-to-contents';
 @Entity({ name: tableName })
+@Unique('UQ_NAMES', ['playlistId', 'contentId'])
+@Check(`"position" > 0`)
+@Check(`"playTime" > 0`)
 export class PlaylistToContent {
   @ApiProperty({ type: String, format: 'uuid' })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @ApiProperty()
-  @Column({ type: 'uuid' })
+  @Column({ name: 'playlistId', type: 'uuid' })
   playlistId: string;
 
   @ApiProperty({ type: String, format: 'uuid' })
-  @Column({ type: 'uuid' })
+  @Column({ name: 'contentId', type: 'uuid' })
   contentId: string;
 
-  @ApiProperty({ type: 'integer' })
+  @ApiProperty({ name: 'position', type: 'integer' })
   @Column({ type: 'integer' })
   position: number;
 
   @ApiProperty()
-  @Column({ type: 'real' })
+  @Column({ name: 'playTime', type: 'real' })
   playTime: number;
 
   //Relations
